472f1197b2c9ba2739887525b163dae045dd4894
orryverducci <orry@orryverducci.co.uk>
2013-08-23 22:42:13 +0100

    Add Mic Live monitoring application

---
+++ MicLive/MainWindow.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace MicLive
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.iconMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ipLabel = new System.Windows.Forms.Label();
+            this.ipTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.inputComboBox = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.statusPanel = new System.Windows.Forms.Panel();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.statusTitleLabel = new System.Windows.Forms.Label();
+            this.iconMenuStrip.SuspendLayout();
+            this.tableLayoutPanel.SuspendLayout();
+            this.statusPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.iconMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Mic Live Control";
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // iconMenuStrip
+            // 
+            this.iconMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openMenuItem,
+            this.exitMenuItem});
+            this.iconMenuStrip.Name = "iconMenuStrip";
+            this.iconMenuStrip.Size = new System.Drawing.Size(104, 48);
+            // 
+            // openMenuItem
+            // 
+            this.openMenuItem.Name = "openMenuItem";
+            this.openMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openMenuItem.Text = "&Open";
+            this.openMenuItem.Click += new System.EventHandler(this.openMenuItem_Click);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitMenuItem.Text = "E&xit";
+            this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
+            // 
+            // ipLabel
+            // 
+            this.ipLabel.AutoSize = true;
+            this.ipLabel.Location = new System.Drawing.Point(12, 15);
+            this.ipLabel.Name = "ipLabel";
+            this.ipLabel.Size = new System.Drawing.Size(47, 13);
+            this.ipLabel.TabIndex = 1;
+            this.ipLabel.Text = "Clock IP";
+            // 
+            // ipTextBox
+            // 
+            this.ipTextBox.Location = new System.Drawing.Point(86, 12);
+            this.ipTextBox.Mask = "990.990.990.990";
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(286, 20);
+            this.ipTextBox.TabIndex = 2;
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(12, 41);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(68, 13);
+            this.inputLabel.TabIndex = 3;
+            this.inputLabel.Text = "Input Device";
+            // 
+            // inputComboBox
+            // 
+            this.inputComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.inputComboBox.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.inputComboBox.FormattingEnabled = true;
+            this.inputComboBox.Location = new System.Drawing.Point(86, 38);
+            this.inputComboBox.Name = "inputComboBox";
+            this.inputComboBox.Size = new System.Drawing.Size(286, 21);
+            this.inputComboBox.TabIndex = 4;
+            this.inputComboBox.SelectedIndexChanged += new System.EventHandler(this.inputComboBox_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.96919F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.03082F));
+            this.tableLayoutPanel.Controls.Add(this.statusPanel, 1, 0);
+            this.tableLayoutPanel.Controls.Add(this.statusTitleLabel, 0, 0);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(15, 65);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(357, 100);
+            this.tableLayoutPanel.TabIndex = 7;
+            // 
+            // statusPanel
+            // 
+            this.statusPanel.BackColor = System.Drawing.Color.Maroon;
+            this.statusPanel.Controls.Add(this.statusLabel);
+            this.statusPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.statusPanel.Location = new System.Drawing.Point(84, 3);
+            this.statusPanel.Name = "statusPanel";
+            this.statusPanel.Size = new System.Drawing.Size(270, 94);
+            this.statusPanel.TabIndex = 0;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.BackColor = System.Drawing.Color.Transparent;
+            this.statusLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.ForeColor = System.Drawing.Color.White;
+            this.statusLabel.Location = new System.Drawing.Point(0, 0);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(270, 94);
+            this.statusLabel.TabIndex = 0;
+            this.statusLabel.Text = "Mic Live";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // statusTitleLabel
+            // 
+            this.statusTitleLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.statusTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusTitleLabel.Location = new System.Drawing.Point(3, 0);
+            this.statusTitleLabel.Name = "statusTitleLabel";
+            this.statusTitleLabel.Size = new System.Drawing.Size(75, 100);
+            this.statusTitleLabel.TabIndex = 1;
+            this.statusTitleLabel.Text = "Status";
+            this.statusTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(384, 175);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Controls.Add(this.inputComboBox);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.ipTextBox);
+            this.Controls.Add(this.ipLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "Mic Live Control";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.iconMenuStrip.ResumeLayout(false);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.statusPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip iconMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem openMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.Label ipLabel;
+        private System.Windows.Forms.MaskedTextBox ipTextBox;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.ComboBox inputComboBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Panel statusPanel;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label statusTitleLabel;
+    }
+}
+
---
+++ MicLive/MainWindow.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpDX;
+using SharpDX.DirectInput;
+
+namespace MicLive
+{
+    public partial class MainWindow : Form
+    {
+        private DirectInput directInput = new DirectInput();
+        private List<DeviceInstance> devices;
+        private Joystick joystick;
+        private Thread inputThread;
+        private bool stopInput = false;
+        bool close = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!close)
+            {
+                e.Cancel = true;
+                notifyIcon.Visible = true;
+                Visible = false;
+            }
+            else
+            {
+                stopInput = true;
+            }
+        }
+
+        private void openMenuItem_Click(object sender, EventArgs e)
+        {
+            DisplayForm();
+        }
+
+        private void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            close = true;
+            Close();
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            DisplayForm();
+        }
+
+        private void DisplayForm()
+        {
+            Visible = true;
+            notifyIcon.Visible = false;
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            devices = (List<DeviceInstance>)directInput.GetDevices(DeviceClass.GameControl, DeviceEnumerationFlags.AttachedOnly);
+            foreach (DeviceInstance device in devices)
+            {
+                inputComboBox.Items.Add(device.InstanceName);
+            }
+            if (inputComboBox.Items.Count > 0)
+            {
+                inputComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void inputComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (joystick != null)
+            {
+                stopInput = true;
+                joystick.Unacquire();
+                joystick = null;
+            }
+            DeviceInstance device = devices.Find(i => i.InstanceName == (string)inputComboBox.SelectedItem);
+            if (device != null)
+            {
+                joystick = new Joystick(directInput, device.InstanceGuid);
+                joystick.Properties.BufferSize = 128;
+                joystick.Acquire();
+            }
+            stopInput = false;
+            inputThread = new Thread(ProcessInput);
+            inputThread.Start();
+        }
+
+        private void ProcessInput()
+        {
+            while (!stopInput)
+            {
+                var stateData = joystick.GetBufferedData();
+                foreach (var state in stateData)
+                {
+                    if (state.Offset == JoystickOffset.Buttons1)
+                    {
+                        if (state.Value > 0)
+                        {
+                            MicLive();
+                        }
+                        else
+                        {
+                            MicOff();
+                        }
+                    }
+                    Console.WriteLine(state);
+                }
+            }
+        }
+
+        private void MicLive()
+        {
+            statusPanel.BackColor = System.Drawing.Color.Red;
+        }
+
+        private void MicOff()
+        {
+            statusPanel.BackColor = System.Drawing.Color.Maroon;
+        }
+    }
+}
