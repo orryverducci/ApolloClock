024613aefcee652df7441969b09f009c70c69651
Orry Verducci <orry@orryverducci.co.uk>
2015-11-19 20:18:24 +0000

    Set app UI colours

--- ApolloClock.Windows/MainPage.xaml.cs
+++ ApolloClock.Windows/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +24,27 @@ namespace ApolloClock.Windows
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        /// <summary>
+        /// Constructor for main page of the application
+        /// </summary>
         public MainPage()
         {
+            // Initialise UI components
             this.InitializeComponent();
+            // Retrieve the application title bar and change its colour
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.BackgroundColor = Color.FromArgb(255, 244, 67, 54);
+            titleBar.InactiveBackgroundColor = Color.FromArgb(255, 244, 67, 54);
+            titleBar.ButtonBackgroundColor = Color.FromArgb(255, 244, 67, 54);
+            titleBar.ButtonInactiveBackgroundColor = Color.FromArgb(255, 244, 67, 54);
+            titleBar.ButtonHoverBackgroundColor = Color.FromArgb(255, 229, 57, 53);
+            titleBar.ButtonPressedBackgroundColor = Color.FromArgb(255, 211, 47, 47);
+            titleBar.ForegroundColor = Colors.White;
+            titleBar.InactiveForegroundColor = Color.FromArgb(255, 255, 205, 210);
+            titleBar.ButtonForegroundColor = Colors.White;
+            titleBar.ButtonInactiveForegroundColor = Color.FromArgb(255, 255, 205, 210);
+            titleBar.ButtonHoverForegroundColor = Colors.White;
+            titleBar.ButtonPressedForegroundColor = Colors.White;
         }
     }
 }
