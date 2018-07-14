3c37edfb1a1b61e122a65e94c9be6da3c3c26177
Orry Verducci <orry@orryverducci.co.uk>
2018-07-14 18:18:45 +0100

    Migrate electron entry point to ES6

---
+++ electron/app.js
@@ -0,0 +1,40 @@
+﻿import {app, BrowserWindow} from "electron";
+import path from "path";
+import url from "url";
+
+// Global reference for the main window, preventing it from being garbage collected
+let mainWindow = null;
+
+// Set app path to the application's root folder
+app.setAppPath(__dirname);
+
+/**
+ * Creates the main application window
+ */
+function createMainWindow() {
+    // Create the main window
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    // Load the main window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "app", "index.html"),
+        protocol: "file:",
+        slashes: true
+    }));
+    // Destroy reference to the main window when it is closed
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+}
+
+// Create the main window when Electron has finished initialization
+app.on("ready", () => {
+    createMainWindow();
+});
+
+// Quit the application when all windows are closed
+app.on("window-all-closed", () => {
+    app.quit();
+});
