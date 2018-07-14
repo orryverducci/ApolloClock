29bdb43228b0a53aee7454d9d693923e636e09bf
Orry Verducci <orry@orryverducci.co.uk>
2018-07-14 18:25:21 +0100

    Setup gulp build system

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+"use strict";
+
+const gulp = require("gulp"),
+    del = require("del"),
+    path = require("path"),
+    sass = require("gulp-sass"),
+    {spawn} = require("child_process"),
+    os = require("os"),
+    vinylPaths = require("vinyl-paths");
+
+/***************
+*** CLEAN TASKS
+****************/
+
+gulp.task("clean:build", (done) => {
+    del.sync(path.join(__dirname, "build"));
+    done();
+});
+
+gulp.task("clean:electron", (done) => {
+    del.sync(path.join(__dirname, "electron", "app"));
+    done();
+});
+
+gulp.task("clean:all", gulp.parallel(
+    "clean:build",
+    "clean:electron"
+));
+
+/***************
+*** BUILD TASKS
+****************/
+
+gulp.task("build:copy", () => {
+    return gulp.src(path.join(__dirname, "src", "**", "*"))
+        .pipe(gulp.dest(path.join(__dirname, "build")));
+});
+
+gulp.task("build:sass", () => {
+    return gulp.src(path.join(__dirname, "build", "**", "*.scss"))
+        .pipe(vinylPaths(del))
+        .pipe(sass({
+            includePaths: [
+                path.join(__dirname, "build")
+            ]
+        }).on("error", sass.logError))
+        .pipe(gulp.dest((file) => {
+            return file.base;
+        }));
+});
+
+gulp.task("build:all", gulp.series(
+    "build:copy",
+    "build:sass"
+));
+
+/*****************
+*** PREPARE TASKS
+******************/
+
+gulp.task("prepare:electron", () => {
+    return gulp.src(path.join(__dirname, "build", "**", "*"))
+        .pipe(gulp.dest(path.join(__dirname, "electron", "app")));
+});
+
+/*************
+*** RUN TASKS
+**************/
+
+gulp.task("run:electron", (done) => {
+    let extension = "";
+    if (os.platform == "win32") {
+        extension = ".bat";
+    }
+    this.process = spawn(path.join(__dirname, "node_modules", ".bin", "electron" + extension), [path.join(__dirname, "electron", "main.js")], {
+        windowsHide: true
+    });
+    done();
+});
